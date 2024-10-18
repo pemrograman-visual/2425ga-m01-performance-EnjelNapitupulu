95c55caf1f923f56092b81385d4d18465a576d46
albertonn <albertonnapitupulu12@gmial.com>
2024-10-18 11:01:23 +0700

    pair-10

--- M01.java
+++ M01.java
@@ -1,2 +1,75 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24056-ENJEL AYUTI NAPITUPULU
+// 12S24033-DOY DENGGAN P.S.
+
+import java.util.*;
+import java.lang.Math;
+
+public class M01{
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double performa, nilaiangka, a, aB, b, bC, c, d, e, kredittotal;
+        String nilaihuruf, kredit;
+
+        kredittotal = 4.0;
+        a = 4.0;
+        aB = 3.5;
+        b = 3.0;
+        bC = 2.5;
+        c = 2.0;
+        d = 1.0;
+        e = 0.0;
+        kredit = input.nextLine();
+        if (kredit.equals("---")) {
+            kredit = kredit;
+        } else {
+            kredittotal = Integer.parseDouble(kredit);
+        }
+        nilaihuruf = input.nextLine();
+        while (!nilaihuruf.equals("---")) {
+            if (nilaihuruf.equals("A")) {
+                nilaiangka = 4.0;
+            } else {
+                if (nilaihuruf.equals("AB")) {
+                    nilaiangka = 3.5;
+                } else {
+                    if (nilaihuruf.equals("B")) {
+                        nilaiangka = 3.0;
+                    } else {
+                        if (nilaihuruf.equals("BC")) {
+                            nilaiangka = 2.5;
+                        } else {
+                            if (nilaihuruf.equals("C")) {
+                                nilaiangka = 2.0;
+                            } else {
+                                if (nilaihuruf.equals("D")) {
+                                    nilaiangka = 1.0;
+                                } else {
+                                    if (nilaihuruf.equals("E")) {
+                                        nilaiangka = 0.0;
+                                    } else {
+                                        nilaiangka = 0.0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            performa = kredittotal * nilaiangka;
+            System.out.println(toFixed(performa,1));
+            kredit = input.nextLine();
+            if (kredit.equals("---")) {
+                kredit = kredit;
+            } else {
+                kredittotal = Integer.parseDouble(kredit);
+            }
+            nilaihuruf = input.nextLine();
+        }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
